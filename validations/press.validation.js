afe760cb19e6e0c84df344adb682f5e313757e4a
Hyder <hyderaliafzal>
2024-08-14 17:05:05 +0500

    cover addition in press api

--- validations/press.validation.js
+++ validations/press.validation.js
@@ -3,6 +3,7 @@ const Joi = require("joi");
 const createPost = {
   body: Joi.object().keys({
     title: Joi.string().trim(),
+    cover: Joi.string().trim(),
     content: Joi.string().trim(),
   }),
 };
