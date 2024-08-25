8a3ef0ef3d5b9185473fb846caf221b1212f0b69
Ali hasan <114381368+A-hasan-code@users.noreply.github.com>
2024-08-25 19:56:09 +0500

    Update index.js

--- validations/index.js
+++ validations/index.js
@@ -1,4 +1,4 @@
 module.exports.userValidation = require("./user.validation");
 module.exports.roleValidation = require("./role.validation");
 module.exports.pressValidation = require("./press.validation");
-module.exports.pressValidation = require("./report.validation");
+module.exports.reportValidation = require("./report.validation");
