50fab7e8d9dee8cb3f66daa66ba92f80c925f47d
TashfeenHayat <tashfeensikanderhayat@gmail.com>
2025-01-21 22:22:27 +0500

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,27 +1,42 @@
 const { Token } = require("../models");
 
 const auth = () => async (req, res, next) => {
-  return new Promise(async (resolve) => {
+  try {
     const { authorization } = req.headers;
-    if (authorization) {
-      const token = authorization.split(" ")[1];
-      const foundToken = await Token.findOne({ token })
-        .populate({
-          path: "userId",
-          populate: {
-            path: "roleId",
-            model: "Role",
-          },
-        })
-        .exec();
-      if (foundToken) {
-        req.user = foundToken.userId;
-        resolve();
-      } else res.status(403).send("Invalid token");
-    } else res.status(401).send("Invalid authorization");
-  })
-    .then(() => next())
-    .catch((err) => next(err));
+
+    if (!authorization) {
+      return res.status(401).send("Authorization header is missing");
+    }
+
+    const token = authorization.split(" ")[1];
+
+    if (!token) {
+      return res.status(401).send("Token is missing");
+    }
+
+    // Check for token in the database
+    const foundToken = await Token.findOne({ token })
+      .populate({
+        path: "userId",
+        populate: {
+          path: "roleId",
+          model: "Role",
+        },
+      })
+      .exec();
+
+    if (!foundToken) {
+      return res.status(403).send("Invalid token");
+    }
+
+    // Attach user information to the request object for further use
+    req.user = foundToken.userId;
+
+    // Continue to the next middleware or route handler
+    next();
+  } catch (err) {
+    return res.status(500).send("Internal server error");
+  }
 };
 
 module.exports = auth;
