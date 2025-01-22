83afdcc94d0721ec4c76c915e4a23d07922d937d
TashfeenHayat <tashfeensikanderhayat@gmail.com>
2025-01-22 13:14:53 +0500

    Update filter.controller.js

--- controllers/filter.controller.js
+++ controllers/filter.controller.js
@@ -67,12 +67,38 @@ const deleteFilter = catchAsync(async (req, res) => {
   }
 });
 
+// const getAllFilters = catchAsync(async (req, res) => {
+//   try {
+//     const { key, limit = 10, page = 1 } = req.query;
+
+//     const query = {};
+//     const skip = (parseInt(page) - 1) * parseInt(limit);
+
+//     // Add search filters to the query object
+//     if (key) {
+//       query.$or = [{ name: { $regex: key, $options: "i" } }];
+//     }
+
+//     // Find total count of filters
+//     const totalCount = await Filter.countDocuments(query);
+//     // If no key is provided, return all agents
+//     const filters = await Filter.find(query)
+//       .limit(parseInt(limit))
+//       .skip(skip)
+//       .sort({ createdAt: -1 });
+//     return res.status(200).json({ filters, totalCount });
+//   } catch (error) {
+//     // Handle errors
+//     console.error(error);
+//     return res.status(500).json("Internal server error");
+//   }
+// });
 const getAllFilters = catchAsync(async (req, res) => {
   try {
-    const { key, limit = 10, page = 1 } = req.query;
+    const { key, limit, page = 1 } = req.query;
 
     const query = {};
-    const skip = (parseInt(page) - 1) * parseInt(limit);
+    const skip = (parseInt(page) - 1) * (parseInt(limit) || 100000); // Default page limit if limit is undefined or null
 
     // Add search filters to the query object
     if (key) {
@@ -81,11 +107,18 @@ const getAllFilters = catchAsync(async (req, res) => {
 
     // Find total count of filters
     const totalCount = await Filter.countDocuments(query);
-    // If no key is provided, return all agents
-    const filters = await Filter.find(query)
-      .limit(parseInt(limit))
+
+    // If limit is provided, apply it. If not, remove the limit condition to get all data
+    const filtersQuery = Filter.find(query)
       .skip(skip)
       .sort({ createdAt: -1 });
+
+    if (limit) {
+      filtersQuery.limit(parseInt(limit));
+    }
+
+    const filters = await filtersQuery;
+
     return res.status(200).json({ filters, totalCount });
   } catch (error) {
     // Handle errors
@@ -93,7 +126,6 @@ const getAllFilters = catchAsync(async (req, res) => {
     return res.status(500).json("Internal server error");
   }
 });
-
 module.exports = {
   createFilter,
   getFilter,
