3cd765b8a477493b771e9f78ab962a8363b294b0
TashfeenHayat <tashfeensikanderhayat@gmail.com>
2025-06-17 05:06:13 +0500

    Update inquiry.controller.js

--- controllers/inquiry.controller.js
+++ controllers/inquiry.controller.js
@@ -32,7 +32,7 @@ const { firstName, lastName, email, message, phoneNumber, propertyId, requestVis
     to: 'Info@FloridaLuxurious.com',
     replyTo: email,
     subject: 'New Contact Us Form Submission',
-    html: html , // fallback plain HTML
+    // fallback plain HTML
     text: `Name: ${firstName} ${lastName}\nEmail: ${email}\nPhone: ${phoneNumber}\nMessage: ${message}` // for clients without HTML support
   };
 
